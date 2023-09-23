0e2a4192e41bcb9de14c4355bf67b933f6081db3
Blake Vollbrecht <blakevollbrecht@gmail.com>
2023-09-23 09:00:03 -0400

    Refactored Clent Component out of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,6 @@
-"use client"
-
-import { useState } from "react"
-import { Map, Marker } from "pigeon-maps"
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts"
-import { format as formatDate } from "date-fns"
-
-import data from "./mock.json"
+import Tractors from "./components/tractors"
 
 export default function Home() {
-  const [current, setCurrent] = useState("")
-
-  function formatTime(time: string) {
-    return formatDate(new Date(time), "h:mm aaa")
-  }
-
   return (
     <main className="flex min-h-screen flex-col p-16">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -41,40 +20,7 @@ export default function Home() {
       </div>
 
       <div className="pt-16">
-        <Map
-          height={500}
-          defaultCenter={[43.51559, -80.60789]}
-          defaultZoom={15}
-        >
-          {data.map((point) => (
-            <Marker
-              className="pl-3"
-              key={point.time}
-              width={20}
-              anchor={[point.lat, point.lng]}
-              onMouseOver={() => setCurrent(point.time)}
-            >
-              <svg
-                className="w-1.5 h-1.5"
-                viewBox="0 0 100 100"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="50" cy="50" r="50" fill="#f00" />
-              </svg>
-            </Marker>
-          ))}
-        </Map>
-        <ResponsiveContainer className="pt-4" height={200}>
-          <LineChart data={data}>
-            <Line type="linear" dataKey="rpm" stroke="#f20" onMouseOver={(x,ev) => console.log(ev.target)} />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="time" tickFormatter={formatTime} />
-            <YAxis
-              label={{ value: "RPM", angle: -90, position: "insideLeft" }}
-            />
-            <Tooltip labelFormatter={formatTime} />
-          </LineChart>
-        </ResponsiveContainer>
+        <Tractors />
       </div>
     </main>
   )
