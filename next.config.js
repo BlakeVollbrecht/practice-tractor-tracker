1098dd7698f96827edd535b662d0e431e6ec2a9c
Blake Vollbrecht <blakevollbrecht@gmail.com>
2023-09-23 10:22:55 -0400

    Revert "Added CI configuration"
    This reverts commit 657a9d63954e63030e00d848af8aa141f1a8826d.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ""
-let basePath = "/"
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  output: "export"
-}
+const nextConfig = { output: "export" }
 
 module.exports = nextConfig
